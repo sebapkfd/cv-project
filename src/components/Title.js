7e1ad072dcc981d3b12c451d067e28fb37ee7300
Seba Soto <sebastian.pkfd@gmail.com>
2020-12-12 01:11:13 -0300

    Repository reference open in new tab

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,9 @@ const Title = () =>{
     return (
         <div className='titleDiv'>
             <h1>CV Project</h1>
-            <a href="https://github.com/sebapkfd/cv-project">Repository</a>
+            <a href="https://github.com/sebapkfd/cv-project" target="_blank" >
+                Repository
+            </a>
         </div>
     )
 }
